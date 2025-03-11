0fb2bc8e3c0a39d53a45541dc180b98e9fb4bbbe
Alex Pendergrass <alexandre.pendergrass@kellogg.northwestern.edu>
2025-03-11 02:29:47 +0000

    set up user stories

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   resources "places"
   resources "sessions"
   resources "users"
+
+  get("/login", {:controller => "sessions", :action => "new"})
+  get("/logout", {:controller => "sessions", :action => "destroy"})
+  
+  # Landing page (aka root route)
+  get("/", {:controller => "sessions", :action => "new"})
 end
