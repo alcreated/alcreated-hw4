9192f827ae7df79f3ea6f373a2e82d95fa937d00
Alex Pendergrass <alexandre.pendergrass@kellogg.northwestern.edu>
2025-03-18 04:46:35 +0000

    updates

---
+++ db/migrate/20250317095809_create_add_username.rb
@@ -0,0 +1,5 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+    def change
+      add_column :places, :user_id, :integer
+    end
+  end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2022_03_09_020701) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_05_095809) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
